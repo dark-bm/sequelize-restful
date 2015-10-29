12c55f80011112329403f728f991b4a08583e575
kaidoe <andycbriggs@gmail.com>
2015-10-29 11:47:30 +0000

    update to support including by association

--- test/router.test.js
+++ test/router.test.js
@@ -82,7 +82,7 @@ describe('Router', function() {
           this.Photo.create({ name: 'b' }).then(function(err) {
             this.Photo.create({ name: 'c' }).then(function(err) {
               this.Photo.create({ name: 'a' }).then(function(err) {
-                this.router.handleRequest({method: 'GET', path: '/api/Photos', query: { where: { name: ['a', 'b', 'c'] }, order: "name ASC" }, body: null}, function(response) {
+                this.router.handleRequest({method: 'GET', path: '/api/Photos', query: { where: { name: ['a', 'b', 'c'] }, order: ['name', 'ASC'] }, body: null}, function(response) {
                   expect(response.status).to.equal('success')
                   expect(response.data.length).to.equal(3)
                   expect(response.data[0].name).to.equal('a')
@@ -108,7 +108,7 @@ describe('Router', function() {
             { name: 'phototest9' }
           ]
           this.Photo.bulkCreate(photoDefinitions).then(function(err) {
-            this.router.handleRequest({method: 'GET', path: '/api/Photos', query: { where: { name: { $like: 'phototest%' } }, order: "name ASC",  offset: 3, limit: 3 }, body: null}, function(response) {
+            this.router.handleRequest({method: 'GET', path: '/api/Photos', query: { where: { name: { $like: 'phototest%' } }, order: ['name', 'ASC'],  offset: 3, limit: 3 }, body: null}, function(response) {
               expect(response.status).to.equal('success')
               expect(response.data.length).to.equal(3)
               expect(response.count).to.equal(9)
